7c4b7ace83ea44ecd039f6a06bea0ae065a8d260
jjom33 <j.jiyeonjung33@gmail.com>
2023-06-08 02:15:35 +0900

    finished exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
 
+
+const findTheOldest = function(people) {
+    var len = people.length;
+    var maxAge = 0;
+    var index = 0;
+    for (let i=0;i<len;i++){
+        let deathYear = people[i].yearOfDeath?people[i].yearOfDeath:2023;
+        let age =deathYear - people[i].yearOfBirth;
+        if ( age > maxAge){
+            maxAge = age;
+            index = i;
+        }
+    }
+    return people[index];
 };
 
-// Do not edit below this line
+// // Do not edit below this line
 module.exports = findTheOldest;

234e9bbc24f1967e3a9425eefbabfe60960363c3
jjom33 <j.jiyeonjung33@gmail.com>
2023-06-08 01:51:16 +0900

    finished exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,27 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (!Number.isInteger(n)){
+        if (!isNaN(n) && Number.isInteger(parseInt(n))){
+            n = parseInt(n);
+        }
+    }
+    
+    if (n <0){
+        return "OOPS";
+    } 
+    
+    if (n==0 || n==1) {
+        return n;
+    }
 
+    let prev = 0;
+    let curr = 1;
+
+    for (let i = 2; i <= n; i++) {
+        let temp = prev + curr;
+        prev = curr;
+        curr = temp;
+  }
+  return curr;
 };
 
 // Do not edit below this line

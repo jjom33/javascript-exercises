bd84e450c39ae4460d27d77789acba2b564c9e39
jjom33 <j.jiyeonjung33@gmail.com>
2023-06-08 01:40:24 +0900

    finished exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    var cleaned = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    var cleandedReversed = cleaned.split('').reverse().join('');
+    return cleaned == cleandedReversed;
+    
 };
 
 // Do not edit below this line

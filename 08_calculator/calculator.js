daeed93672510fdc04fb11320554791d6cc2ece3
jjom33 <j.jiyeonjung33@gmail.com>
2023-06-08 01:29:38 +0900

    finished exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  var sum =0;
+	for (let i=0;i<arr.length;i++){
+    sum += arr[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  var multipled =1;
+	for (let i=0;i<arr.length;i++){
+    multipled *= arr[i];
+  }
+  return multipled;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	if (x==0 || x== 1){
+    return 1;
+  }
+  else{
+    return x * factorial(x-1);
+  }
 };
 
 // Do not edit below this line

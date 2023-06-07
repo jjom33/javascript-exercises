eb4c554083d89e1a9adba9b2d1205a6cd31b8b87
jjom33 <j.jiyeonjung33@gmail.com>
2023-06-08 01:20:39 +0900

    finished exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let celsius = (fahrenheit - 32) * 5/9;
+  celsius= Math.ceil(celsius * 10) / 10;
+
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let fahrenheit = (celsius * 9/5) + 32;
+  fahrenheit = Math.ceil(fahrenheit * 10) / 10;
+  return fahrenheit;
 };
 
 // Do not edit below this line

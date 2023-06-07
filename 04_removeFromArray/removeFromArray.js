ba58a3c9b5f011471fe18f1e1da2db487a37a91f
jjom33 <j.jiyeonjung33@gmail.com>
2023-06-08 00:50:50 +0900

    finished exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
 const removeFromArray = function() {
-
+    const argsArray = [...arguments];
+    var arr = argsArray[0];
+    var elements = argsArray.slice(1);
+    for (let i=0; i<elements.length; i++){
+        let index = arr.indexOf(elements[i]);
+        if (index !== -1) {
+            arr.splice(index, 1);
+          }
+    }
+    return arr;
 };
 
 // Do not edit below this line

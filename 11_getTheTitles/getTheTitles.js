27fc532effb20153610afc08424e09ca9a24d3b7
jjom33 <j.jiyeonjung33@gmail.com>
2023-06-08 01:55:31 +0900

    finished exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    var len = books.length;
+    var result =[];
+    for (let i=0;i<len;i++){
+        result.push(books[i].title);
+    }
+    return result;
 };
 
 // Do not edit below this line
